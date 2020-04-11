b06cf4c028e815667a6880765af15482973fdedc
guilhermerfs00 <guilherme.rfs00@gmail.com>
2020-04-11 13:01:38 -0300

    Produtos sendo adicioando removidos e alterados

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -5,21 +5,29 @@ import 'package:lojavirtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartModel extends Model {
-
   UserModel user;
 
   List<CartProduct> product = [];
 
-  CartModel(this.user);
+  CartModel(this.user) {
+    if (user.isLoggedIn()) {
+      _loadCartItens();
+    }
+  }
 
   bool isLoading = false;
 
-
-  static CartModel of(BuildContext context) => ScopedModel.of<CartModel>(context);
+  static CartModel of(BuildContext context) =>
+      ScopedModel.of<CartModel>(context);
 
   void addCartItem(CartProduct cartProduct) {
     product.add(cartProduct);
-    Firestore.instance.collection('users').document(user.firebaseUser.uid).collection('cart').add(cartProduct.toMap()).then((doc) {
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .add(cartProduct.toMap())
+        .then((doc) {
       cartProduct.cid = doc.documentID;
     });
 
@@ -27,9 +35,51 @@ class CartModel extends Model {
   }
 
   void removeCartItem(CartProduct cartProduct) {
-    Firestore.instance.collection('users').document(user.firebaseUser.uid).collection('cart').document(cartProduct.cid).delete();
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .document(cartProduct.cid)
+        .delete();
 
     product.remove(cartProduct);
+    notifyListeners();
+  }
+
+  void addProduct(CartProduct cartProduct) {
+    cartProduct.quantity++;
+
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+    notifyListeners();
+  }
+
+  void decProduct(CartProduct cartProduct) {
+    cartProduct.quantity--;
+
+    Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .document(cartProduct.cid)
+        .updateData(cartProduct.toMap());
+    notifyListeners();
+  }
+
+  void _loadCartItens() async {
+    QuerySnapshot query = await Firestore.instance
+        .collection('users')
+        .document(user.firebaseUser.uid)
+        .collection('cart')
+        .getDocuments();
+
+    product =
+        query.documents.map((doc) => CartProduct.fromDocument(doc)).toList();
+
     notifyListeners();
   }
 }
--- lib/tiles/cart_tile.dart
+++ lib/tiles/cart_tile.dart
@@ -2,103 +2,116 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/datas/cart_product.dart';
 import 'package:lojavirtual/datas/product_data.dart';
+import 'package:lojavirtual/models/cart_model.dart';
 
 class CartTile extends StatelessWidget {
-
   final CartProduct cartProduct;
 
   CartTile(this.cartProduct);
 
   @override
   Widget build(BuildContext context) {
-
     Widget _buildContent() {
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
               padding: EdgeInsets.all(8.0),
-              width: 120.0,
-              child: Image.network(
-                cartProduct.productData.images[0],
-                fit: BoxFit.cover,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    cartProduct.productData.title,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                  ),
+                  Text(
+                    'Tamanho : ${cartProduct.size}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    'R\$ ${cartProduct.productData.price.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.remove),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: cartProduct.quantity > 1
+                              ? () {
+                                  CartModel.of(context).decProduct(cartProduct);
+                                }
+                              : null),
+                      Text(
+                        cartProduct.quantity.toString(),
+                      ),
+                      IconButton(
+                          icon: Icon(Icons.add),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () {
+                            CartModel.of(context).addProduct(cartProduct);
+                          }),
+                      FlatButton(
+                        child: Text(
+                          "Remover",
+                        ),
+                        textColor: Colors.grey,
+                        onPressed: () {
+                          CartModel.of(context).removeCartItem(cartProduct);
+                        },
+                      )
+                    ],
+                  )
+                ],
               ),
             ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      cartProduct.productData.title,
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 17.0
-                      ),
-                    ),
-                    Text(
-                      'Tamanho : ${cartProduct.size}',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    Text(
-                      'R\$ ${cartProduct.productData.price.toStringAsFixed(2)}',
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 16.0,
-                        fontWeight: FontWeight.w300,
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        IconButton(
-                            icon: Icon(Icons.remove),
-                            color: Theme.of(context).primaryColor,
-                            onPressed: cartProduct.quantity > 1 ? (){} : null
-                            ),
-                        Text(cartProduct.quantity.toString(),),
-                        IconButton(
-                            icon: Icon(Icons.add),
-                            color: Theme.of(context).primaryColor,
-                            onPressed: (){}),
-
-                        FlatButton(
-                          child: Text("Remover",),
-                          textColor: Colors.grey,
-                          onPressed: (){} ,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        );
+          )
+        ],
+      );
     }
 
     return Card(
-      margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-      child: cartProduct.productData == null ?
-        FutureBuilder<DocumentSnapshot> (
-          future: Firestore.instance.collection('products').document(cartProduct.category).collection('itens').document(cartProduct.pid).get(),
-          builder: (context, snapshot){
-            if(snapshot.hasData) {
-              cartProduct.productData = ProductData.fromDocument(snapshot.data);
-              return _buildContent();
-            } else {
-              return Container(
-                height: 70.0,
-                child: CircularProgressIndicator(),
-                alignment: Alignment.center,
-              );
-            }
-          },
-        ) : _buildContent()
-    );
+        margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: cartProduct.productData == null
+            ? FutureBuilder<DocumentSnapshot>(
+                future: Firestore.instance
+                    .collection('products')
+                    .document(cartProduct.category)
+                    .collection('items')
+                    .document(cartProduct.pid)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    cartProduct.productData =
+                        ProductData.fromDocument(snapshot.data);
+                    return _buildContent();
+                  } else {
+                    return Container(
+                      height: 70.0,
+                      child: CircularProgressIndicator(),
+                      alignment: Alignment.center,
+                    );
+                  }
+                },
+              )
+            : _buildContent());
   }
 }
